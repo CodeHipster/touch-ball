84fdbc9f761cc1de8b44c3980aeccd07230b6c16
thijs oostdam <thijs@oostd.am>
2024-03-11 21:00:18 +0100

    adding dragging sounds

--- modules/gestures.mjs
+++ modules/gestures.mjs
@@ -1,7 +1,7 @@
 import { random_color } from "./colors.mjs";
 import { paint_circle } from "./painter.mjs";
 import { clamp_circle, get_circle, get_circle_by_coord, move_circle } from "./circle_manager.mjs";
-import { tap } from "./audio.mjs";
+import { drag, tap } from "./audio.mjs";
 
 // disable default gestures for touch screens
 document.addEventListener('touchstart', function (event) {
@@ -17,6 +17,8 @@ export function setup_gestures(canvas, circle) {
   canvas.addEventListener("touchmove", on_touch_move(canvas.getContext("2d")));
 }
 
+// TODO: detect stopping moving...
+
 function on_touch_move(ctx) {
   return (event) => {
     // console.log("touch move", event)
@@ -28,13 +30,26 @@ function on_touch_move(ctx) {
       if (circle_touch) {
         const x = touch.clientX + circle_touch.offset.x
         const y = touch.clientY + circle_touch.offset.y
-        move_circle(circle_touch.circle, x, y)
         const circle = get_circle(circle_touch.circle)
+        const prevX = circle.x
+        const prevy = circle.y
+        move_circle(circle_touch.circle, x, y)
+        drag(circle_touch.circle, calculate_speed(prevX, x, prevy, y, circle_touch.time, event.timeStamp ))
+        circle_touch.time = event.timeStamp
         clamp_circle(circle, ctx.canvas.width, ctx.canvas.height)
         paint_circle(circle, ctx)
       }
     }
   }
+}
+
+// speed in pixels? / ms
+function calculate_speed(x1, y1, x2, y2, timestamp1, timestamp2){
+  // console.log(`x1:${x1},x2:${x2},y1:${y1},y2:${y2},t1:${timestamp1},t2:${timestamp2}`)
+  const xDiff = x2-x1
+  const yDiff = y2-y1
+  const dist = Math.sqrt(xDiff*xDiff + yDiff*yDiff)
+  return dist / (timestamp2 - timestamp1)
 }
 
 function on_touch_end() {
@@ -67,7 +82,7 @@ function on_touch_start(ctx) {
         }
         const x_offset = circle.x - x;
         const y_offset = circle.y - y;
-        circle_touches[touch_id] = { offset: { x: x_offset, y: y_offset }, circle: circle.id }
+        circle_touches[touch_id] = { offset: { x: x_offset, y: y_offset }, circle: circle.id, time:event.timeStamp }
         // console.log("setting circle touch", circle_touches[touch_id])
 
         ctx.fillStyle = random_color();
---
+++ processors/noise-generator.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A noise generator with a gain AudioParam.
+ *
+ * @class NoiseGenerator
+ * @extends AudioWorkletProcessor
+ */
+class NoiseGenerator extends AudioWorkletProcessor {
+
+  constructor(){
+      super();
+      console.log("log?");
+  }
+
+  static get parameterDescriptors() {
+    return [
+      {name: 'amplitude', defaultValue: 0.5, minValue: 0, maxValue: 1},
+      {name: 'velocity', defaultValue: 0, minValue: 0, maxValue: 1},
+    ];
+  }
+
+  process(inputs, outputs, parameters) {
+      console.log("ping");
+    const output = outputs[0];
+    const amplitude = parameters.amplitude;
+    const isAmplitudeConstant = amplitude.length === 1;
+    const velocity = parameters.velocity;
+    const isVelocityConstant = velocity.length === 1;
+
+    for (let channel = 0; channel < output.length; ++channel) {
+      const outputChannel = output[channel];
+      for (let i = 0; i < outputChannel.length; ++i) {
+        // multiplying the sound by a random value between 0-1 to the power of 100 (which gives very low values unless the random value is close to 1)
+        // causes the random noise to be quieter, but with the occasional snap.
+        // to generate more snaps/time we need to increase the odds of the random number to be close to 1.
+        // to do this we set the speed range from 1 to 10, divide the random number by the speed, and add 1-1/speed. 
+        // To for example set the range from .9 to 1. example: (0.5 / 10) + 1-1/10 = 0.05 + 0.9 = 0.95
+        let chanceToAmplify = 0;
+        const v = (isVelocityConstant ? velocity[0] : velocity[i])
+        if(v != 0){
+          const s = v * 100 + 1
+          chanceToAmplify = (Math.random() / s) + (1-1/s);
+        }
+        // This loop can branch out based on AudioParam array length
+        outputChannel[i] = 2 * (Math.random() - 0.5)
+         * Math.pow(chanceToAmplify,1000)
+          *  (isAmplitudeConstant ? amplitude[0] : amplitude[i]);
+      }
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('noise-generator', NoiseGenerator);
